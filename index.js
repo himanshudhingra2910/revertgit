3fc3025bce18eaabc7a12533e4e3aae788b493fc
himanshudhingra2910 <himanshudhingra2910@gmail.com>
2024-08-14 20:26:34 +0530

    javascript added

---
+++ index.js
@@ -0,0 +1,17 @@
+// console.log("Hello World");
+// Array= [1,2,3,4,5];
+// let obj=[
+    // 1,2,3,4,5,6,67
+// ];
+// console.log(obj)
+console.log(inputString);
+var inputString=  "Name";
+
+let contactNo= "Do not contact.";
+contactNo= "Contact pls";
+
+const pin = 560067
+
+// console.log(inputString);
+console.log(contactNo);
+console.log(pin);
